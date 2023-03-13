80e358aecb2c136b99e0cced8829da1300d7143b
Mehrdad Ghannad <mehrdad37ghannad@gmail.com>
2023-03-13 00:04:20 -0400

    add all prod endpoints

--- frontend/client/src/config.js
+++ frontend/client/src/config.js
@@ -1,7 +1,11 @@
 const prod = {
     env: 'production',
     api_host: 'https://csc301-378115-backend-4ic67og2pa-pd.a.run.app', // an empty string to signify a relative path. can also put a deployment URL.
-    endpoints: {text_replace: '/anon/text-replace'}
+    endpoints: {
+        text_replace: '/anon/text-replace', 
+        text_file_replace: '/anon/text-file-replace',
+        csv_file_replace: '/anon/csv-file-replace',
+    },
 };
 
 const dev = {
