f15cb3b39c10f5657edeb4cea2a8a0cdc5b864fd
Nathan Hansen <nathanajhansen@gmail.com>
2023-03-20 14:00:16 -0400

    auto lint with black

--- backend/backend/anonymizer/app.py
+++ backend/backend/anonymizer/app.py
@@ -8,16 +8,18 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, resources={r"*": {"origins": "*"}})
-api = swagger.docs(Api(app), apiVersion='0.0.3')
+api = swagger.docs(Api(app), apiVersion="0.0.3")
 
-@app.route('/')
+
+@app.route("/")
 def render_directory():
-    return render_template('index.html')
+    return render_template("index.html")
 
-api.add_resource(TXTReplace, '/anon/text-replace')
-api.add_resource(TXTFileReplace, '/anon/text-file-replace')
-api.add_resource(CSVFileReplace, '/anon/csv-file-replace')
+
+api.add_resource(TXTReplace, "/anon/text-replace")
+api.add_resource(TXTFileReplace, "/anon/text-file-replace")
+api.add_resource(CSVFileReplace, "/anon/csv-file-replace")
 # adding more endpoints here
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- backend/backend/anonymizer/resources/TXTFileReplace.py
+++ backend/backend/anonymizer/resources/TXTFileReplace.py
@@ -8,49 +8,45 @@ from werkzeug.datastructures import FileStorage
 from common.util import textReplace
 
 parser = reqparse.RequestParser()
-parser.add_argument('inputTextFile', type=FileStorage, location='files')
+parser.add_argument("inputTextFile", type=FileStorage, location="files")
 # this endpoint will take a .txt file rather than a string
-parser.add_argument('replaceTerms', location='form')
+parser.add_argument("replaceTerms", location="form")
+
 
 class TXTFileReplace(Resource):
     "Simple text replace anonymization tool for TXT files"
+
     @swagger.operation(
-        responseClass='text',
+        responseClass="text",
         parameters=[
             {
                 "name": "inputTextFile",
                 "description": "TXT file that the user wises to anonymize",
                 "required": True,
                 "allowMultiple": False,
-                "dataType": 'text'
+                "dataType": "text",
             },
             {
                 "name": "replaceTerms",
                 "description": "Dictionary of all all key-term pairs that the user wishes to anonymize",
                 "required": False,
                 "allowMultiple": False,
-                "dataType": 'Dict'
-            }
-            ],
-        responseMessages=[
-            {
-                "code": 200,
-                "message": "[ANONYMIZED USER TXT FILE]"
+                "dataType": "Dict",
             },
-            {
-                "code": 400,
-                "message": "missing parameter(s)"
-            }
-            ]
-        )
+        ],
+        responseMessages=[
+            {"code": 200, "message": "[ANONYMIZED USER TXT FILE]"},
+            {"code": 400, "message": "missing parameter(s)"},
+        ],
+    )
     def post(self):
         args = parser.parse_args()
-        inputTextFile = args['inputTextFile']
-        replaceTerms = eval(args['replaceTerms'])
+        inputTextFile = args["inputTextFile"]
+        replaceTerms = eval(args["replaceTerms"])
 
         print(inputTextFile)
         if not inputTextFile or not replaceTerms or inputTextFile == "":
-            return {'message': 'missing parameter(s)'}, 400
+            return {"message": "missing parameter(s)"}, 400
 
         # get input text from the txt file
         inputText = inputTextFile.read()
@@ -59,10 +55,10 @@ class TXTFileReplace(Resource):
         outputText = textReplace(decoded_inputText, replaceTerms)
 
         tmp = tempfile.TemporaryFile()
-        b = bytes(outputText, 'utf-8')
+        b = bytes(outputText, "utf-8")
         tmp.write(b)
         tmp.seek(0)
-    
-        return send_file(tmp, attachment_filename='anonymized.txt')
 
-        #return jsonify(inputTextFile)
+        return send_file(tmp, attachment_filename="anonymized.txt")
+
+        # return jsonify(inputTextFile)
