f0f9c8805d1592d0154cb65bf239623cc8875360
Jonathan <jonathanginevro@outlook.com>
2023-03-22 17:29:16 -0400

    updated start scripts

--- backend/start_prod.sh
+++ backend/start_prod.sh
@@ -3,7 +3,7 @@
 pip install pipenv
 python3 -m virtualenv venv
 source venv/bin/activate
-pip install -r backend/requirements.txt
+pip install -r requirements.txt
 sudo ufw allow 5000
-cd backend/anonymizer/
+cd anonymizer/
 uwsgi --socket 127.0.0.1:5000 --protocol=http -w wsgi:app
