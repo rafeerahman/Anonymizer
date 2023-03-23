903214fd5b70fc17e8c097c58ca2bf37faf924d9
sayna <sayna.sohrabzadeh@mail.utoronto.ca>
2023-03-23 19:08:45 -0400

    added huggingface model to the utils file to use in experimental endpoint

--- backend/anonymizer/common/util.py
+++ backend/anonymizer/common/util.py
@@ -1,5 +1,8 @@
 # this is where all the common infrastructure will go
 import re
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline
+
 
 
 def textReplace(inputText: str, replaceTerms: dict) -> str:
@@ -16,3 +19,25 @@ def textReplace(inputText: str, replaceTerms: dict) -> str:
     )
 
     return outputText
+
+def huggingface_model(inputTxt: str):
+    tokenizer = AutoTokenizer.from_pretrained("Davlan/bert-base-multilingual-cased-ner-hrl")
+    model = AutoModelForTokenClassification.from_pretrained("Davlan/bert-base-multilingual-cased-ner-hrl")
+    nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+    ner_results = nlp(inputTxt)
+    print("this is the result:\n")
+    print(ner_results)
+    #make a for loop to add all tokens of orginization and persons together
+    replace = []
+    count_r = -1
+    for r in ner_results:
+        if r['word'][0] == '#' and count_r > -1:
+            word = r['word'][2:]
+            replace[count_r] += word
+        else:
+            count_r += 1
+            replace.append(r['word'])
+    print(replace)
+
+
+huggingface_model( "Nader Jokhadar had given Syria the lead with a well-struck header in the seventh minute.")
