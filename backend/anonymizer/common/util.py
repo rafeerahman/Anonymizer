395282f994ea92610ec404d893da9d6b1eca5744
Jonathan <jonathanginevro@outlook.com>
2023-03-25 14:19:49 -0400

    disallow textReplace to anonymize substrings

--- backend/anonymizer/common/util.py
+++ backend/anonymizer/common/util.py
@@ -7,15 +7,17 @@ def textReplace(inputText: str, replaceTerms: dict) -> str:
     replaceTerms = {key.lower(): value for key, value in replaceTerms.items()}
 
     # ensure that regex is not picked up from any replacement terms
-    refined = ((re.escape(key), value) for key, value in replaceTerms.items())
+    refined = (("\\b"+re.escape(key)+"\\b", value) for key, value in replaceTerms.items())
     replaceTerms = dict(refined)
+    
+    print(replaceTerms)
 
     # create regex replacement pattern
     replacePattern = re.compile("|".join(replaceTerms.keys()), re.IGNORECASE)
 
     # apply the pattern, swapping from replacement dictionary
     outputText = replacePattern.sub(
-        lambda m: replaceTerms[re.escape(m.group(0)).lower()], inputText
+        lambda m: replaceTerms["\\b"+re.escape(m.group(0)).lower()+"\\b"], inputText
     )
 
     return outputText
