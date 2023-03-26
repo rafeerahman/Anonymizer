b75a9c5e7f7ea9003a84f8e26d93336b977990e7
Mehrdad Ghannad <mehrdad37ghannad@gmail.com>
2023-03-26 19:00:20 -0400

    fixed question mark button on smaller screen sizes

--- frontend/client/src/components/AutoButton.js
+++ frontend/client/src/components/AutoButton.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import InfoModal from './InfoModal';
 import Col from 'react-bootstrap/Col';
@@ -9,34 +9,31 @@ export default function AutoButton({useAuto, setUseAuto}){
         <>
           <Row>
             <Col>
-              <BtnStyled>
+              <Buttons>
                 <label className="custom-example">
-                <input
-                  id="toggle-check"
-                  type="checkbox"
-                  checked={useAuto}
-                  onChange={() => {
-                    setUseAuto(!useAuto);
-                    
-                  }}
-                />
-                {useAuto ? "Disable Auto Replace": "Enable Auto replace"}
-                
+                  <input
+                    id="toggle-check"
+                    type="checkbox"
+                    checked={useAuto}
+                    onChange={() => {
+                      setUseAuto(!useAuto);
+                    }}
+                  />
+                  {useAuto ? "Disable Auto Replace": "Enable Auto replace"}
                 </label>
-              </BtnStyled>
-            </Col>
-            <Col xs = {2}>
-              <InfoModal/>
+                <InfoModal/>
+              </Buttons>
             </Col>
           </Row>
-          
         </>
       )
   }
   
-  const BtnStyled = styled.div`
+  const Buttons = styled.div`
       display: inline;
       position: relative;
+      display: flex;
+      justify-content: space-between;
   
       input {
           display: none;
@@ -53,6 +50,11 @@ export default function AutoButton({useAuto, setUseAuto}){
           justify-content: center;
           cursor: pointer;
           margin-bottom: 1rem;
+      }
+
+      label {
+          width: 100%;
+          margin-right: 1rem;
       }
   `
   
--- frontend/client/src/components/InfoModal.js
+++ frontend/client/src/components/InfoModal.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import styled from 'styled-components';
 
 export default function InfoModal() {
   const [show, setShow] = useState(false);
@@ -10,11 +11,13 @@ export default function InfoModal() {
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        ? 
-      </Button>
+      <BtnStyled>
+        <button variant="primary" onClick={handleShow}>
+          ? 
+        </button>
+      </BtnStyled>
 
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose} centered>
         <Modal.Header closeButton>
           <Modal.Title>What is Auto Replace?</Modal.Title>
         </Modal.Header>
@@ -29,4 +32,25 @@ export default function InfoModal() {
   );
 }
 
-// render(<InfoModal />);
+const BtnStyled = styled.div`
+    display: inline;
+    position: relative;
+
+    input {
+        display: none;
+    }
+
+    button {
+        width: 34px;
+        height: 34px;
+        background-color: #5436DA;
+        color: white;
+        border-radius: 10px;
+        font-weight: 600;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        margin-bottom: 1rem;
+    }
+`
