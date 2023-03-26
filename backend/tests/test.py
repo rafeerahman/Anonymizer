59b1d1e9fe5606e096be98d36b94b28930fe2144
Nathan Hansen <nathanajhansen@gmail.com>
2023-03-26 16:19:01 -0400

    fix linting

--- backend/tests/test.py
+++ backend/tests/test.py
@@ -8,6 +8,9 @@ from backend.anonymizer.common.util import textReplace
 
 
 def test_textReplace():
-    inputTxt = 'Hello my name is Bob Sam, and I work for Gogle'
-    terms = {'Bob':'Name1', 'Sam': 'Name2', 'Gogle': 'Organization1'}
-    assert textReplace(inputTxt, terms) == 'Hello my name is Name1 Name2, and I work for Organization1'
+    inputTxt = "Hello my name is Bob Sam, and I work for Gogle"
+    terms = {"Bob": "Name1", "Sam": "Name2", "Gogle": "Organization1"}
+    assert (
+        textReplace(inputTxt, terms)
+        == "Hello my name is Name1 Name2, and I work for Organization1"
+    )
