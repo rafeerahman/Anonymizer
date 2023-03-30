bf4acd2f97064cfd25e0d7080a850b983b573eef
Jonathan <jonathanginevro@outlook.com>
2023-03-30 00:18:52 -0400

    more code styling

--- backend/anonymizer/test.py
+++ backend/anonymizer/test.py
@@ -38,9 +38,11 @@ class TestTXTReplace(unittest.TestCase):
 
     def test_anonymize_text_no_parameters(self):
         data = json.dumps({})
-        response = self.app.post('/api/anonymize/text', content_type='application/json', data=data)
+        response = self.app.post(
+            "/api/anonymize/text", content_type="application/json", data=data
+        )
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json, {'message': 'Invalid input'})
+        self.assertEqual(response.json, {"message": "Invalid input"})
 
 
 class TestCSVFileReplace(unittest.TestCase):
@@ -50,20 +52,20 @@ class TestCSVFileReplace(unittest.TestCase):
     def test_anonymize_csv_success(self):
         csv_content = "John Doe,XYZ company\nJane Doe,ABC company"
         replaceTerms = {
-            "John Doe": "Person A", 
-            "Jane Doe": "Person B", 
-            "XYZ company": "Company A", 
-            "ABC company": "Company B"
+            "John Doe": "Person A",
+            "Jane Doe": "Person B",
+            "XYZ company": "Company A",
+            "ABC company": "Company B",
         }
 
         data = {
-            "inputFile": (io.BytesIO(csv_content.encode("utf-8")), "test.csv"), 
-            "replaceTerms": json.dumps(replaceTerms)
+            "inputFile": (io.BytesIO(csv_content.encode("utf-8")), "test.csv"),
+            "replaceTerms": json.dumps(replaceTerms),
         }
         response = self.app.post(
             "/api/anonymize/file/csv", content_type="multipart/form-data", data=data
         )
-        
+
         self.assertEqual(response.status_code, 200)
         self.assertEqual(
             response.data.decode("utf-8"), "Person A,Company A\nPerson B,Company B\n"
@@ -90,8 +92,8 @@ class TestTXTFileReplace(unittest.TestCase):
         replaceTerms = {"John Doe": "Person A", "XYZ company": "Company A"}
 
         data = {
-            "inputTextFile": (io.BytesIO(txt_content.encode("utf-8")), "test.txt"), 
-            "replaceTerms": json.dumps(replaceTerms)
+            "inputTextFile": (io.BytesIO(txt_content.encode("utf-8")), "test.txt"),
+            "replaceTerms": json.dumps(replaceTerms),
         }
         response = self.app.post(
             "/api/anonymize/file/txt", content_type="multipart/form-data", data=data
