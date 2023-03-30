03409248243c1a17e0725d1ee044dadec7e8ec8a
Nathan Hansen <nathanajhansen@gmail.com>
2023-03-30 13:26:22 -0400

    make credit card regex more aggressive

--- backend/anonymizer/common/util.py
+++ backend/anonymizer/common/util.py
@@ -16,7 +16,7 @@ def regexReplace(inputText: str, replaceTerms: dict):
     # dictionary of supported regex replacement
     regex_library = {
         "phone_number": r"\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})",
-        "credit_card": r"^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$",
+        "credit_card": r"(([0-9][ \-,]?){15})[0-9]",
         "postal_code": r"[ABCEGHJKLMNPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][ ]?\d[ABCEGHJ-NPRSTV-Z]\d",
     }
 
