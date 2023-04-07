1f92c36ed5a1a3ca0264b0b12a664b4693485535
ed29leung <edwardleung.ca@gmail.com>
2023-04-06 23:51:34 -0400

    Added load auto text example to example button.

--- frontend/client/src/components/ExampleOrResetButton.js
+++ frontend/client/src/components/ExampleOrResetButton.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function ExampleOrResetButton({
-    text, setText, file, setFile, resetParams, autoReplaceTerms,
+    text, setText, file, setFile, resetParams, useAuto, setReplaceDict, setSwitchDict, autoReplaceTerms,
     setAutoReplaceTerms, replaceTerms, setReplaceTerms, setCurrentFileType,
     setResponseText
 }){
@@ -32,22 +32,57 @@ export default function ExampleOrResetButton({
                   // erase the currently existing file and parameters 
                   resetParams();
                   setReplaceTerms({}); 
-
-                  // populate based on example
-                  setText("My name is Jack, and you can reach me at 647-123-4321");
-                  setReplaceTerms({"Jack": "NAME", "647-123-4321": "XXX-XXX-XXXX"});
-                  setCurrentFileType({
-                    displayName: "Text Replace",
-                    URL: "endpoint-url-for/text-replace",
-                    fileType: "text/plain"
-                  });
-                  setChecked(true)
+                  setAutoReplaceTerms({});
+                  if (useAuto){
+                    setText(`Dear Admissions Committee, \n \nI am writing to express my interest in applying for admission to the University of Toronto for the upcoming academic year. I am a highly motivated student with a passion for Computer Science, and I believe that the University of Toronto would be the perfect place for me to pursue my academic and professional goals. \n \
+                    \nHere is my personal information:
+                    \nName: John Smith
+                    \nAddress: 1234 Main Street, Toronto, Ontario, M64 3H6
+                    \nPhone Number: 555-123-4567
+                    \nCredit Card Number: 4637 2737 2791 0533
+                    \nCurrent Highschool: Cedar Valley Institute
+                    
+                    \nIn addition to my personal information, I have included the following documents as part of my application:
+                    \nTranscripts from Cedar Valley Institute, showing my grades and academic achievements to date
+                    \nLetters of recommendation from Bob Sally, Jane Doe, and Sarah Lee, who can attest to my academic abilities and personal character
+                    \nAn essay outlining my academic and career goals, and why I believe that University of Toronto is the ideal place for me to achieve them
+                    \nAny additional documentation or information that may be required by your admissions committee
+                    \nI believe that my academic achievements, extracurricular activities, and personal qualities make me an excellent candidate for admission to the University of Toronto. I am eager to learn from your distinguished faculty, engage with my peers in meaningful discussions and collaborations, and contribute to the vibrant and diverse community that the University of Toronto fosters.
+                    \nThank you for considering my application. I look forward to hearing back from you soon. \n\nSincerely, \nJohn Smith`)
+                    setSwitchDict({"names": true, "location": true, "org": true, "phone_number": true, "postal_code": true, "credit_card": true})
+                    setReplaceDict( {
+                      "names":  "name",
+                      "location": "location",
+                      "org": "organization",
+                      "phone_number": "phone-number",
+                      "postal_code": "postal-code",
+                      "credit_card": "credit-card"
+                      }
+                    )
+                    setCurrentFileType({
+                      displayName: "Text Replace",
+                      URL: "endpoint-url-for/text-replace",
+                      fileType: "text/plain"
+                    });
+                    setChecked(true);
+                  }
+                  else{
+                    // populate based on example
+                    setText("My name is Jack, and you can reach me at 647-123-4321");
+                    setReplaceTerms({"Jack": "NAME", "647-123-4321": "XXX-XXX-XXXX"});
+                    setCurrentFileType({
+                      displayName: "Text Replace",
+                      URL: "endpoint-url-for/text-replace",
+                      fileType: "text/plain"
+                    });
+                    setChecked(true)
+                }
                 }
               }}
             />
             {checked ||text.length || file || 
               Object.keys(replaceTerms).length || Object.keys(autoReplaceTerms).length
-              ? "Clear": "Load Text Example"}
+              ? "Clear": useAuto? "Load Auto Text Example" : "Load Text Example"}
           </label>
         </BtnStyled>
       </>
